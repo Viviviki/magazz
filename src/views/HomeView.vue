84ff432bce5d63c4e507b71124f5bc7308ba2681
Viviviki <viktor.avronin@yandex.ru>
2024-12-26 14:06:30 +0500

    изменение данных на HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GoodsGrid />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import GoodsGrid from '@/components/GoodsGrid.vue';
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    GoodsGrid
   }
 }
 </script>
